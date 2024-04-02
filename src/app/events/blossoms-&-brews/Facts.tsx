e35131f1be50639ac23e3cf9dcd85a929f439712
Agustin <agustindevelops@gmail.com>
2024-04-01 22:25:20 -0500

    Dress Code

--- src/app/events/blossoms-&-brews/Facts.tsx
+++ src/app/events/blossoms-&-brews/Facts.tsx
@@ -1,24 +1,3 @@
-import { BLOSSOMS_AND_BREWS } from '@/utils/content/events';
-import {
-  CloudArrowUpIcon,
-  LockClosedIcon,
-  ServerIcon,
-} from '@heroicons/react/20/solid';
-
-const features = [
-  {
-    name: 'WHEN: ',
-    description:
-      'July 2024 on a sunday two time frames 11am-12:30pm; 1pm-2:30pm',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'WHERE: ',
-    description: 'San Antonio: Exact Location TBD',
-    icon: LockClosedIcon,
-  },
-];
-
 const Facts = () => {
   return (
     <div className='font-pacifico text-peach-500 text-shadow-md px-4 text-center text-5xl md:text-8xl'>
