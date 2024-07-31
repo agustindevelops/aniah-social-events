8716d717541bfeb5f08ea191d994c1f073ccb9d5
Agustin <agustindevelops@gmail.com>
2024-07-30 20:05:08 -0500

    buildfix

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,10 +1,5 @@
-import React, { FC, ReactNode } from 'react';
 import PlanningCoordination from 'src/app/services/components/PlanningCoordination';
-interface ServiceLayoutProps {
-  children: ReactNode;
-}
-
-const Services: FC<ServiceLayoutProps> = ({ children }) => {
+const Services = () => {
   return (
     <div className='layout container my-24 grid gap-2 p-2'>
       <PlanningCoordination />
