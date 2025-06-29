a2db1bf477b4edcc9682056a2fa941b7fd9c7022
Agustin <agustindevelops@gmail.com>
2025-06-29 17:23:51 -0500

    media fallback

--- src/app/blog/[slug]/Media.tsx
+++ src/app/blog/[slug]/Media.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from "react";
+import Image from "next/image";
+import { FC } from "react";
 
-type PropTypes = { video: string; image: string };
+type PropTypes = { video?: string; image?: string; alt?: string };
 
-const Media: FC<PropTypes> = ({ video, image }) => {
+const Media: FC<PropTypes> = ({ video, image, alt }) => {
   return (
     <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
       <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
@@ -19,10 +20,12 @@ const Media: FC<PropTypes> = ({ video, image }) => {
             Your browser does not support the video tag.
           </video>
         ) : image ? (
-          <img
+          <Image
             className="absolute inset-0 h-full w-full bg-gray-50 object-cover object-bottom"
             src={image}
-            alt=""
+            alt={alt || "featured image for blog post"}
+            width={1000}
+            height={1000}
           />
         ) : (
           <></>
