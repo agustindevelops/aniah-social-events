945d81fbdecac0dd7fada3f29d1e63f3aef0f61b
Agustin <agustindevelops@gmail.com>
2025-07-23 21:24:11 -0500

    build issue fix

---
+++ src/app/schedule/Calendly.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { InlineWidget } from "react-calendly";
+
+const Calendly = () => (
+  <InlineWidget url="https://calendly.com/aniahsocialevents/30min?back=1" />
+);
+
+export default Calendly;
